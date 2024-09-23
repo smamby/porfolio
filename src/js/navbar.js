f5c1b26c8d172030eab6b78ffe7f825af2673375
Seba Mamby <s.mamby@gmail.com>
2024-09-23 01:21:33 -0300

    0.04 trabajando en home, nav bar hecha, estudiar mas el concepto, seguir buscando

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,10 +1,10 @@
-const navbar = `<div class=cabezal></div><nav>
+const navbar = `<nav>
                 <ul>
                     <li> <a href="./index.html#Home">Home</a> </li>
                     <li> <a href="./index.html#SobreMi">Sobre Mi</a> </li>
                     <li> <a href="./index.html#Conocimientos TI">Conocimiesntos TI</a> </li>
                 </ul>
-            </nav><div class=cabezal></div>`;
+            </nav>`;
 
 
 //function navbarMount() {
